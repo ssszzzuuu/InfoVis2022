1cb31b1ec2888b600f4ded182bf2ed338902787d
ssszzzuuu <103481259+ssszzzuuu@users.noreply.github.com>
2022-05-22 18:48:38 +0900

    Update w10_task1_main.js

--- W10/w10_task1_main.js
+++ W10/w10_task1_main.js
@@ -1,4 +1,4 @@
-d3.csv("https://ssszzzuuu.github.io/InfoVis2022/W08/data.csv")
+d3.csv("https://ssszzzuuu.github.io/InfoVis2022/W10/data.csv")
 .then( data => {
     data.forEach( d => { d.value = +d.value });
     
